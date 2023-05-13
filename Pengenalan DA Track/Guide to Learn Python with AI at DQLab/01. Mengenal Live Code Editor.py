3bbbbfc5bc519c3e148f0c02bb654d907fc98514
Anwar12234 <128627819+Anwar12234@users.noreply.github.com>
2023-05-13 15:26:41 +0900

    Update 01. Mengenal Live Code Editor.py

--- Pengenalan DA Track/Guide to Learn Python with AI at DQLab/01. Mengenal Live Code Editor.py
+++ Pengenalan DA Track/Guide to Learn Python with AI at DQLab/01. Mengenal Live Code Editor.py
@@ -1,1 +1,21 @@
-Live Code Editor (LCE) adalah fitur belajar yang sedang Anda gunakan saat ini. Fitur ini berbeda dengan bagian sebelumnya. Tujuan fitur ini adalah untuk menunjang pengalaman belajar yang baik dengan cara praktik coding dan mendapatkan feedback langsung. LCE sendiri adalah fitur yang sangat lengkap agar Anda mendapatkan pengalaman belajar coding yang terbaik, namun tetap sederhana. Hal ini bertujuan untuk memudahkan pemula dalam mengikutinya, sementara juga tetap nyaman digunakan oleh yang sudah berpengalaman.
+Live Code Editor (LCE) adalah fitur belajar yang sedang Anda
+gunakan saat ini. Fitur ini berbeda dengan bagian sebelumnya.
+Tujuan fitur ini adalah untuk menunjang pengalaman belajar yang
+baik dengan cara praktik coding dan mendapatkan feedback
+langsung. LCE sendiri adalah fitur yang sangat lengkap agar Anda
+mendapatkan pengalaman belajar coding yang terbaik, namun
+tetap sederhana. Hal ini bertujuan untuk memudahkan pemula
+dalam mengikutinya, sementara juga tetap nyaman digunakan
+oleh yang sudah berpengalaman.
+
+LCE terdiri dari tiga panel utama:
+
+file:///C:/Users/Away/Downloads/download.png
+    
+1. Panel Materi
+Panel materi ini terdiri dari dua tab, yaitu Theory dan Hint.
+Tab Theory berisi teks yang memberikan penjelasan tentang
+topik yang sedang difokuskan dan juga menyediakan soal
+praktek. Sementara itu, bagian Hint berisi petunjuk
+penyelesaian soal praktek jika Anda menekan tombol Hint.
+Tombol tersebut terletak di bawah LCE.
